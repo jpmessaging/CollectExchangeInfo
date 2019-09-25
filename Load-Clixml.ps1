fa7a75c00c12667b58de756b390df372d98ad26d
Ryusuke Fujita <49670803+RyusukeFujita@users.noreply.github.com>
2019-09-26 05:01:10 +0900

    Added progress report

--- Load-Clixml.ps1
+++ Load-Clixml.ps1
@@ -1,81 +1,72 @@
-<#
-  Load all Clixml files in a folder
-  if Path is not specified, it uses the current directory
-#>
-function Load-Clixml
-{
+function Load-CliXml {
     #requires -Version 3.0
     [CmdletBinding()]
-    param
-    (
-    [Parameter(Mandatory=$true)]
-    $Path,
-    [Switch]$SkipVariableCreation
+    param (
+        [Parameter(Mandatory=$true)]
+        $Path,
+        [Switch]$SkipVariableCreation
     )
 
-    # Resolve Path
     $resolvedPath = Resolve-Path $Path -ErrorAction SilentlyContinue
-    if (!$resolvedPath)
-    {        
-        Write-Error "$Path doesn't exit"
-        return
+    if (!$resolvedPath) {
+        throw "$Path doesn't exit"
     }
 
     Write-Verbose "Path is $resolvedPath"
 
     # Process each file
     $sw = [System.Diagnostics.StopWatch]::StartNew()
-    $files = Get-ChildItem $resolvedPath -Filter "*.xml"    
-    
+    $files = Get-ChildItem $resolvedPath -Filter "*.xml"
+
     $importedObjects = New-Object System.Collections.Generic.List[object]
     $importedFileCount = 0
     $failedFiles = @(
-        foreach ($file in $files)
-        {                    
-            try
-            {
+        foreach ($file in $files) {
+            try {
                 $importedValue = Import-Clixml -Path $file.FullName -ErrorAction:Stop
                 $importedFileCount++
 
                 # if SkipVariableCreation, then imported objects are outputted as 'ImportedObjects'.
-                if ($SkipVariableCreation)
-                {                    
-                    $importedObjects.Add($importedValue)                    
+                if ($SkipVariableCreation) {
+                    $importedObjects.Add($importedValue)
                 }
-                else
-                {
+                else {
                     # Check if the variable exist already
                     $variableName = $file.BaseName
                     $variable = Get-Variable $variableName -Scope Global -ErrorAction SilentlyContinue
 
                     # if already exist, remove it.
-                    if ($variable)
-                    {
+                    if ($variable) {
                         Write-Verbose "Removing $variableName"
-                        Remove-Variable $variableName -Scope Global                
+                        Remove-Variable $variableName -Scope Global
                     }
 
                     # Create a variable in Global Scope and set the value
                     New-Variable -Name $variableName -Scope Global -Value $importedValue
                     Write-Verbose "$variableName is created"
+                    Write-Progress -Activity "Loading CliXml" -Status "$importedFileCount/$($files.Count) files loaded." -PercentComplete:($importedFileCount/$files.Count*100)
                 }
             }
-            catch [System.Xml.XmlException],[System.Security.Cryptography.CryptographicException] 
-            {
+            catch [System.Xml.XmlException],[System.Security.Cryptography.CryptographicException] {
                 Write-Verbose "Cannot import $($file.FullName)"
                 Write-Output $($file.FullName)
             }
         }
+        Write-Progress -Activity "Loading CliXml" -Completed
     )
 
     $sw.Stop()
-    
-    if ($SkipVariableCreation -and ($importedFileCount -ne $importedObjects.Count))
-    {
+
+    if ($SkipVariableCreation -and ($importedFileCount -ne $importedObjects.Count)) {
         Write-Warning "[BUG] importedFileCount ($importedFileCount) doesn't match importedObjects.Count ($($importedObjects.Count))."
     }
 
-    Write-Output (
-        [PSCustomObject]@{VariablesCreated = !$SkipVariableCreation; ImportedFileCount = $importedFileCount; ImportedObjects = $importedObjects; FailedFileCount = $failedFiles.Count ;FailedFiles = $failedFiles; Elapsed = $sw.Elapsed}
-    )
+    [PSCustomObject]@{
+        VariablesCreated = !$SkipVariableCreation;
+        ImportedFileCount = $importedFileCount;
+        ImportedObjects = $importedObjects;
+        FailedFileCount = $failedFiles.Count;
+        FailedFiles = $failedFiles;
+        Elapsed = $sw.Elapsed
+    }
 }
